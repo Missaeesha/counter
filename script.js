93bc37c19db7d236ddcc3526000e655629769b30
Missaeesha <aishatambali4@gmail.com>
2021-12-17 11:30:38 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const counterValue = document.querySelector('#counter-value');
+const btns = document.querySelectorAll('.btn');
+let count = 0;
+
+btns.forEach(element => {
+        element.addEventListener('click', () => {
+         if(element.classList.contains('decrease')){count--;
+         }else if(element.classList.contains('increase')){count++;  
+         }else if(element.classList.contains('reset')){count = 0;
+         }if(count <= 0){
+             counterValue.style.color = ('red');
+         }if(count == 0){
+             counterValue.style.color = ('pink');
+         }if(count >= 0){
+             counterValue.style.color = ('green');
+         }
+
+         counterValue.innerHTML = count;
+
+        });
+})
