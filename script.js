db25d31071d7e9794258ed13f9aecd804084fed4
Missaeesha <aishatambali4@gmail.com>
2022-01-01 19:36:20 -0500

    git cimmit

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ btns.forEach(element => {
          if(element.classList.contains('decrease')){count--;
          }else if(element.classList.contains('increase')){count++;  
          }else if(element.classList.contains('reset')){count = 0;
-         }if(count <= 0){
+         }if(count < 0){
              counterValue.style.color = ('red');
          }if(count == 0){
              counterValue.style.color = ('pink');
-         }if(count >= 0){
+         }if(count > 0){
              counterValue.style.color = ('green');
          }
 
